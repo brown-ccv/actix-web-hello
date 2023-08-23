75685ca053ab857813c71d1f15288c2f1d7f2543
Paul Stey <paulstey@gmail.com>
2023-08-22 22:04:47 -0400

    feat: add a new route that adds 1 to things

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(greeting)
 }
 
+async fn add_one(num: web::Path<String>) -> impl Responder {
+    let result = 1 + num.parse::<i128>().expect("Failed to parse `num` argument");
+    HttpResponse::Ok().body(result.to_string())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = "8080";
@@ -15,6 +20,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/{name}", web::get().to(greet))
+            .route("/add/{num}", web::get().to(add_one))   
+
     })
     .bind(ip_address)?
     .run()
